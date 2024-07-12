309718f415af1c8af01f149c9c6e010a2053bdb6
High23 <warenasb2@gmail.com>
2024-07-11 22:59:18 -0700

    Moved create post component from user repo

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import NavBar from "../navbar/navbar"
 
-export default function EditPost() {
+export function EditPost() {
     const token = localStorage.getItem('token')
     const [oldPost, setOldPost] = useState(null)
     const [error, setError] = useState(null)
@@ -90,6 +91,98 @@ function EditPostForm({token, oldPost, params}) {
 }
 
 function PostUpdateErrors({errors}) {
+    return (
+        <div> Post creation error(s):
+            {errors.map((err, index) => {
+                return (
+                    <div key={index}>
+                        <span>{err.msg}</span>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export function CreatePost() {
+    const [token, setToken] = useState(localStorage.getItem('token'))
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        if (token === null) {
+            setError("You do not have a token!")
+            return
+        }
+        async function verifyAuthorStatus() {
+            const response = await fetch(import.meta.env.VITE_SITEURL + "post/create", {
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            });
+            if (response.status === 500) {
+                setError("An error has occurred fetching this page. You either do not have access or a valid token!")
+            }
+        }
+        verifyAuthorStatus()
+    }, [])
+
+    return (
+        <>
+            <NavBar token={token}></NavBar>
+            {error === null ? 
+            <CreateForm token={token}></CreateForm>
+            : <>{error}</>}
+        </>
+    )
+}
+
+function CreateForm({token}) {
+    const [errors, setErrors] = useState(null);
+    const navigate = useNavigate()
+
+    async function formSubmission(form) {
+        form.preventDefault();
+        const formData = new FormData(form.target);
+        const data = `title=${formData.get('postTitle')}&text=${formData.get('postText')}&published=${formData.get('publishCheckBox')}`;
+        const response = await fetch(import.meta.env.VITE_SITEURL + "post/create", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Bearer " + token,
+            },
+            body: data,
+        });
+        const info = await response.json();
+        if (info.errors) {
+            setErrors(info.errors);
+            return
+        }
+        navigate(`/user`, {replace: true})
+    }
+
+    return (
+        <>
+            <form action="" method='post' onSubmit={(form) => {formSubmission(form)}}>
+                <div>
+                    <label htmlFor="postTitle">Title: </label>
+                    <input type="text" name="postTitle" id="postTitle" autoComplete='off' />
+                </div>
+                <div>
+                    <label htmlFor="postText">Text: </label>
+                    <textarea type="text" name="postText" id="postText" cols={120} rows={30}  />
+                </div>
+                <div>
+                    <label htmlFor="publishCheckBox">Publish: </label>
+                    <input type="checkbox" name="publishCheckBox" id="publishCheckBox"/>
+                </div>
+                <button>Create post</button>
+            </form>
+            {errors !== null && <PostCreationErrors errors={errors}></PostCreationErrors>}
+        </>
+    )
+}
+
+
+function PostCreationErrors({errors}) {
     return (
         <div> Post creation error(s):
             {errors.map((err, index) => {
