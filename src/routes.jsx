dd7602842aed89071a5bd64e08fda7e7290f24a3
High23 <warenasb2@gmail.com>
2024-07-11 22:57:12 -0700

    Added more authoring routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,27 @@
 import ErrorPage from "./components/errorpage/errorpage.jsx";
-import { CurrentUser } from "./components/user/user.jsx";
-import EditPost from "./components/post/post.jsx";
+import { CurrentUser, EditCurrentUser } from "./components/user/user.jsx";
+import { EditPost, CreatePost } from "./components/post/post.jsx";
+import LogIn from "./components/login/login.jsx";
 
 const routes = [
     {
         path: "/",
+        element: <LogIn></LogIn>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
+    {
+        path: "/user",
         element: <CurrentUser></CurrentUser>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
+    {
+        path: "/user/:userId/edit",
+        element: <EditCurrentUser></EditCurrentUser>,
+        errorElement: <ErrorPage></ErrorPage>,
+    },
+    {
+        path: "/post/create",
+        element: <CreatePost></CreatePost>,
         errorElement: <ErrorPage></ErrorPage>,
     },
     {
